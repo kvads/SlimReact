f2675abd4407e036151001ecd4e7eb85e026cce4
Kirill Vasyukov <kvasyukov99@gmail.com>
2022-08-09 00:35:36 +0500

    08/08/22 symfony console and docker containers optimization

--- api/public/index.php
+++ api/public/index.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Psr\Container\ContainerInterface;
+use Slim\App;
 
 http_response_code(500);
 require __DIR__ . '/../vendor/autoload.php';
@@ -9,5 +10,6 @@ require __DIR__ . '/../vendor/autoload.php';
 /** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
+/** @var App $app */
 $app = (require __DIR__ . '/../config/app.php') ($container);
 $app->run();
--- api/vendor/composer/autoload_psr4.php
+++ api/vendor/composer/autoload_psr4.php
@@ -7,6 +7,14 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
+    'Symfony\\Polyfill\\Php73\\' => array($vendorDir . '/symfony/polyfill-php73'),
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Polyfill\\Intl\\Normalizer\\' => array($vendorDir . '/symfony/polyfill-intl-normalizer'),
+    'Symfony\\Polyfill\\Intl\\Grapheme\\' => array($vendorDir . '/symfony/polyfill-intl-grapheme'),
+    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
+    'Symfony\\Contracts\\Service\\' => array($vendorDir . '/symfony/service-contracts'),
+    'Symfony\\Component\\String\\' => array($vendorDir . '/symfony/string'),
+    'Symfony\\Component\\Console\\' => array($vendorDir . '/symfony/console'),
     'Slim\\Psr7\\' => array($vendorDir . '/slim/psr7/src'),
     'Slim\\' => array($vendorDir . '/slim/slim/Slim'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
